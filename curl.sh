207371eeedacc5dd9842fffc61b6f7e0ff018651
Mike Turner <mturner@realinteractive.ca>
2016-07-06 11:22:58 -0400

    added some formating to output

--- curl.sh
+++ curl.sh
@@ -1,8 +1,14 @@
 #! /bin/sh
 
+echo '=================================='
+echo $(date)
+echo '=================================='
+
 echo '-- change into user_generated directory'
 cd /var/www/vhosts/ristaging.ca/contest-templates.ristaging.ca/library/images/user_generated
 
 echo '-- find and copy files to server'
 # find . -type f -exec curl --user 'EBNOBackups:H@3f&$fh%ef' -o --ftp-create-dirs -T {} ftp://216.86.147.25:234/images/{} \;
 find . -type f -exec curl --user 'RITemplatesBackups:P@#$HNhdu$45hd' -o --ftp-create-dirs -T {} ftp://216.86.147.25:235/images/{} \;
+
+echo ''
